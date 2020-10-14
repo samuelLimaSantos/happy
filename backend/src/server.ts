a20bdff365492c2ed9db372474a138ff453bb4b1
Samuel Santos <samuellima280499@gmail.com>
2020-10-14 01:40:00 -0300

    Add database

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import './database/connection';
+import path from 'path';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.listen(3333, () => {
+  console.log('Server is running at port 3333 🚀');
+});
