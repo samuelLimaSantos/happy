198631dd8d8f53db310a65c941fea4959be92b5a
Samuel Santos <samuellima280499@gmail.com>
2020-10-15 22:59:49 -0300

    Added backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,22 @@
 import express from 'express';
+import 'express-async-errors';
 import './database/connection';
+import cors from 'cors';
 import path from 'path';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use(routes);
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log('Server is running at port 3333 🚀');
